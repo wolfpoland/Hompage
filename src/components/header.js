f800afcf35228fa5e7cee23708d14bc1d9e414fa
wolfpoland <wolfpoland1@gmail.com>
2019-03-08 11:12:00 +0100

    added react-springs for animaiton on page

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,13 @@ import React from "react"
 import styled from "styled-components"
 
 const Container = styled.header`
-  background: #f3fafb;
+  position: fixed;
+  width: 100%;
+  background-color: rgba(243,250,251,0.5);
   height: 10vh;
 `
 
 const Content = styled.div`
-  margin: 0 auto;
   max-width: 960px;
   padding: 1.45rem 1.0875rem;
 `
